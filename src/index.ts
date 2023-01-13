c9f9953ea02c128e3ae2e5d0213ec4b12b9c8bd2
Sebastian T F <sebastian.tharakan97@gmail.com>
2023-01-13 18:39:38 +0530

    feat: 🚧 add sample multisend estimate

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,32 @@
-import { Greeter } from './lib/greeter';
+import { Wallet, ethers } from 'ethers';
+import { estimateGas } from './lib/estimateGas';
+import { Multisend__factory } from '../types';
+import { config } from './common/config';
+import { MULTISEND_ADDRESSES } from './common/constants';
+import { parseEther } from 'ethers/lib/utils';
 
-console.log(Greeter('Joe'));
+(async function () {
+  const provider = new ethers.providers.JsonRpcProvider(config.rpcUrl);
+
+  const senderAddress = '0xFB5b21C1d090D40A29cd7BB9BbE3eBA9e8f06b91';
+  const multisendContract = Multisend__factory.connect(
+    MULTISEND_ADDRESSES[(await provider.getNetwork()).chainId],
+    provider
+  );
+
+  const tokenAddress = '0xe8B3dFEE339ce4954A7300E8d421A4BA2F51e7b0';
+  const recipients = [
+    Wallet.createRandom().address,
+    Wallet.createRandom().address,
+    Wallet.createRandom().address,
+  ];
+  const amounts = [parseEther('10'), parseEther('14'), parseEther('20')];
+
+  const targetTxn = await multisendContract.populateTransaction.multisendToken(
+    tokenAddress,
+    recipients,
+    amounts
+  );
+
+  await estimateGas(senderAddress, targetTxn);
+})();
