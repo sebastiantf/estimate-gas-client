97031f30e029d3ef70c92cf7fc78cdc930a21821
Sebastian T F <sebastian.tharakan97@gmail.com>
2023-01-25 12:38:22 +0530

    feat: ✨ use `calculateAllowanceStorageKeyFromAccessList()`

--- src/allowance.ts
+++ src/allowance.ts
@@ -2,6 +2,7 @@ import { ethers } from 'ethers';
 import { config } from './common/config';
 import { MULTISEND_ADDRESSES } from './common/constants';
 import {
+  calculateAllowanceStorageKeyFromAccessList,
   calculateAllowanceStorageKeyFromSlot,
   getAllowanceStateDiff,
 } from './lib/allowance';
@@ -11,12 +12,33 @@ import {
   const tokenAddress = '0xe8B3dFEE339ce4954A7300E8d421A4BA2F51e7b0';
   const fromAddress = '0xFB5b21C1d090D40A29cd7BB9BbE3eBA9e8f06b91';
   const toAddress = MULTISEND_ADDRESSES[(await provider.getNetwork()).chainId];
+
   const allowanceSlot = 2; // Allowance slot (differs from contract to contract)
-  const storageKey = calculateAllowanceStorageKeyFromSlot(
+  const storageKeyFromSlot = calculateAllowanceStorageKeyFromSlot(
     fromAddress,
     toAddress,
     allowanceSlot
   );
+  console.log(
+    '🚀 ~ file: allowance.ts:21 ~ storageKeyFromSlot',
+    storageKeyFromSlot
+  );
 
-  await getAllowanceStateDiff(tokenAddress, fromAddress, toAddress, storageKey);
+  const storageKeyFromAccessList =
+    await calculateAllowanceStorageKeyFromAccessList(
+      fromAddress,
+      toAddress,
+      tokenAddress
+    );
+  console.log(
+    '🚀 ~ file: allowance.ts:27 ~ storageKeyFromAccessList',
+    storageKeyFromAccessList
+  );
+
+  await getAllowanceStateDiff(
+    tokenAddress,
+    fromAddress,
+    toAddress,
+    storageKeyFromAccessList
+  );
 })();
